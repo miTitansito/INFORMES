95899d9ca31f9b1f7c5db04d772aab09e84207c8
Janeth Pinzon Solano <bpinzon@unal.edu.co>
2022-11-19 22:19:16 -0500

    parcial

---
+++ INFORME3/control.py
@@ -0,0 +1,19 @@
+import binario
+import decimal
+import hexadecimal
+import octal
+
+
+
+
+print('---------Conversor Numerico-------')
+print("   ")
+print("Recuerde que debe ingresar el numero a convertir correctamente, en su forma correcta")
+print("   ")
+print("Escriba el numero que desea convetir")
+print("   ")
+print(" 1.Decimal \n 2.Octal \n 3.Hexadecimal \n 4.Binario")
+print("Escriba el numero al que desea convetir")
+print("   ")
+print(" 1.Decimal \n 2.Octal \n 3.Hexadecimal \n 4.Binario")
+

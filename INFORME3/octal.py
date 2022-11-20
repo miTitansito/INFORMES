5548cdbc43fe02537cef8b6cc04c45e5bb66dd44
Janeth Pinzon Solano <bpinzon@unal.edu.co>
2022-11-19 19:00:46 -0500

    parcial

---
+++ INFORME3/octal.py
@@ -0,0 +1,6 @@
+
+""" El modulo Octal debe tener las siguientes funciones:
+         convertirABinario     => recibe string (cadena_octal), retorna string (cadena_binario)
+         convertirADecimal     => recibe string (cadena_octal), retorna string (cadena_decimal)
+         convertirAHexadecimal => recibe string (cadena_octal), retorna string (cadena_hexadecimal)"""
+         
